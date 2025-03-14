efd6748b62d86e1f2554ae839e7797607d7db1b4
cgy <100090733+cgyinthehouse@users.noreply.github.com>
2025-03-14 11:33:18 +0800

    20250314
    1. add _vimrc for VSCodeVim
    2. update Brewfile
    3. tmux: remove the C-a second prefix
    4. fish: add orbstack

---
+++ _vimrc
@@ -0,0 +1,6 @@
+nmap <leader>h :nohl<cr>
+nmap <leader>lf editor.action.formatDocument
+nmap gr editor.action.goToReferences
+nmap gi editor.action.goToImplementation
+nmap K gh
+nmap gpd editor.action.peekDefinition
--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -213,3 +213,7 @@ end
 
 # Added by Windsurf
 fish_add_path /Users/kentsmba/.codeium/windsurf/bin
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
